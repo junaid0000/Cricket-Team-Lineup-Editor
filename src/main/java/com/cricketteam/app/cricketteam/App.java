2723f085d13b60468614de438fb80dc175c12efd
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-06-27 15:11:16 +0200

    initializing git and project directory

---
+++ src/main/java/com/cricketteam/app/cricketteam/App.java
@@ -0,0 +1,13 @@
+package com.cricketteam.app.cricketteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
