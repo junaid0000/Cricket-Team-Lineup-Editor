2c8ee7b398912073a508cfe8dec5febbd0c529e3
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-06-29 14:41:50 +0200

    Implement updatePlayer in Controller and View Interface Passing Test

--- src/main/java/com/cricketteam/app/cricketteam/controller/PlayerController.java
+++ src/main/java/com/cricketteam/app/cricketteam/controller/PlayerController.java
@@ -29,6 +29,7 @@ public class PlayerController {
     }
 
     public void updatePlayer(Player player) {
-        
+        playerRepository.update(player);
+        playerView.playerUpdated(player);
     }
 }
