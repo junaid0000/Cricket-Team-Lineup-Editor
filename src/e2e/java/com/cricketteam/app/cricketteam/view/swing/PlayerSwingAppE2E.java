269f9318da27a57824f9afcbc1e77a76ae75e885
Junaid <64260129+junaid0000@users.noreply.github.com>
2026-07-07 00:52:12 +0200

    Add E2E Tests with Swing and Testcontainers

---
+++ src/e2e/java/com/cricketteam/app/cricketteam/view/swing/PlayerSwingAppE2E.java
@@ -0,0 +1,145 @@
+package com.cricketteam.app.cricketteam.view.swing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import javax.swing.JFrame;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.cricketteam.app.cricketteam.model.Player;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
+@RunWith(GUITestRunner.class)
+public class PlayerSwingAppE2E extends AssertJSwingJUnitTestCase {
+
+    @ClassRule
+    public static final MongoDBContainer mongo = new MongoDBContainer("mongo:5");
+
+    private static final String CRICKET_DB_NAME = "cricket";
+    private static final String PLAYER_COLLECTION_NAME = "player";
+
+    private FrameFixture window;
+    private MongoClient client;
+    private MongoCollection<Document> playerCollection;
+
+    @Override
+    protected void onSetUp() {
+        client = MongoClients.create(mongo.getReplicaSetUrl());
+        client.getDatabase(CRICKET_DB_NAME).drop();
+        playerCollection = client
+            .getDatabase(CRICKET_DB_NAME)
+            .getCollection(PLAYER_COLLECTION_NAME);
+
+        application("com.cricketteam.app.cricketteam.app.swing.CricketTeamSwingApp")
+            .withArgs(
+                "--mongo-host=" + mongo.getHost(),
+                "--mongo-port=" + mongo.getFirstMappedPort(),
+                "--db-name=" + CRICKET_DB_NAME,
+                "--db-collection=" + PLAYER_COLLECTION_NAME)
+            .start();
+
+        window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+            @Override
+            protected boolean isMatching(JFrame frame) {
+                return "Cricket Team LineUp".equals(frame.getTitle()) && frame.isShowing();
+            }
+        }).using(robot());
+    }
+
+    @After
+    public void closeMongoClient() {
+        client.close();
+    }
+
+    @Test
+    @GUITest
+    public void testAddButtonSuccess() {
+        fillPlayerFields("1", "Junaid Munir", "Batsman");
+        window.button(JButtonMatcher.withText("Add")).click();
+        assertListContainsExactly(new String[] { "1 - Junaid Munir - Batsman" });
+        assertThat(findPlayerById("1"))
+            .usingRecursiveComparison()
+            .isEqualTo(new Player("1", "Junaid Munir", "Batsman"));
+    }
+
+    @Test
+    @GUITest
+    public void testUpdateButtonSuccess() {
+        addTestPlayerToDatabase("1", "Junaid Munir", "Batsman");
+        // We do not have a Refresh button, but the app fetches all players on startup
+        // wait for the UI to show the player
+        window.list("playerList").requireItemCount(1);
+
+        window.list("playerList").selectItem(0);
+        window.textBox("nameTextBox").deleteText().enterText("Junaid M");
+        window.textBox("roleTextBox").deleteText().enterText("Captain");
+        window.button(JButtonMatcher.withText("Update")).click();
+        
+        assertListContainsExactly(new String[] { "1 - Junaid M - Captain" });
+        assertThat(findPlayerById("1"))
+            .usingRecursiveComparison()
+            .isEqualTo(new Player("1", "Junaid M", "Captain"));
+    }
+
+    @Test
+    @GUITest
+    public void testDeleteButtonSuccess() {
+        addTestPlayerToDatabase("1", "Junaid Munir", "Batsman");
+        window.list("playerList").requireItemCount(1);
+
+        window.list("playerList").selectItem(0);
+        window.button(JButtonMatcher.withText("Delete")).click();
+        assertListContainsExactly(new String[] {});
+        assertThat(findPlayerById("1"))
+            .isNull();
+    }
+
+    private void fillPlayerFields(String id, String name, String role) {
+        window.textBox("idTextBox").setText("");
+        window.textBox("idTextBox").enterText(id);
+        window.textBox("nameTextBox").setText("");
+        window.textBox("nameTextBox").enterText(name);
+        window.textBox("roleTextBox").setText("");
+        window.textBox("roleTextBox").enterText(role);
+    }
+
+    private void assertListContainsExactly(String[] expectedContents) {
+        assertThat(window.list("playerList").contents())
+            .containsExactly(expectedContents);
+    }
+
+    private void addTestPlayerToDatabase(String id, String name, String role) {
+        playerCollection.insertOne(
+            new Document()
+                .append("id", id)
+                .append("name", name)
+                .append("role", role));
+    }
+
+    private Player findPlayerById(String id) {
+        Document playerDocument = playerCollection.find(Filters.eq("id", id)).first();
+        if (playerDocument == null) {
+            return null;
+        }
+        return new Player(
+            "" + playerDocument.get("id"),
+            "" + playerDocument.get("name"),
+            "" + playerDocument.get("role"));
+    }
+}
